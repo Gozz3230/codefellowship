b362d3af61cd0d3717c2b51c50f1e83a6a54fa9c
Ryan Gosselin <ryan.gosselin5@gmail.com>
2023-11-07 23:05:51 +0000

    spring generated

---
+++ codefellowshipRyanJava401d18/src/main/java/com/codefellowshipRyanJava401d18/codefellowshipRyanJava401d18/CodefellowshipRyanJava401d18Application.java
@@ -0,0 +1,13 @@
+package com.codefellowshipRyanJava401d18.codefellowshipRyanJava401d18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipRyanJava401d18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipRyanJava401d18Application.class, args);
+	}
+
+}
---
+++ codefellowshipRyanJava401d18/src/test/java/com/codefellowshipRyanJava401d18/codefellowshipRyanJava401d18/CodefellowshipRyanJava401d18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefellowshipRyanJava401d18.codefellowshipRyanJava401d18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipRyanJava401d18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
